aa7f712aeddf98a4fe482d2f3e995423f94bf7e5
Muhammad-Zaid-Yousaf <zaid.yousaf@venturenox.com>
2023-11-05 22:16:06 +0500

    Done some changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Nav from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
