3f47072636f577e3f3981e549967cc6190965c07
Muhammad-Zaid-Yousaf <zaid.yousaf@venturenox.com>
2023-12-03 18:16:16 +0500

    vide player

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -5,13 +5,17 @@ import Table from "../../Components/Dashboard/Table";
 import PlanCards from "../../Components/Plans/PlanCards";
 import Nav from "../../Components/Navbar";
 import Footer from "../../Components/Footer";
+import VideoPlayer from "../../Components/Common/VideoPlayer";
 
 function Dashboard() {
+  const videoUrl = "https://247c.mrgamingstreams.com/mrgamingnhlnet.m3u8";
+
   return (
     <>
       <Nav />
       <div>
         <DashHeader title="DashBoard" SubTitle="DashBoard" />
+        <VideoPlayer url={videoUrl} />
         <Plans />
         <Table />
         <Footer />
