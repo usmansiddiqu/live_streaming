4651e7e64475bebf5aab8125d328d195bf8959ae
DawoodAli23 <tatwatatwa007@gmail.com>
2023-12-07 20:46:48 +0500

    deleted file

--- src/api/categories.api.js
+++
@@ -1,9 +0,0 @@
-const { default: axiosInstance } = require(".");
-const getCategories = async () => {
-  const result = await axiosInstance.get("/category/getAllCategories");
-  console.log(result);
-  return result;
-};
-module.exports = {
-  getCategories,
-};
