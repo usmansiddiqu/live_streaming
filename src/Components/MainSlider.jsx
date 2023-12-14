c0f23f2e4af1f477b3517b30f7c86582ea9d29b7
Muhammad-Zaid-Yousaf <zaid.yousaf@venturenox.com>
2023-12-14 23:27:53 +0500

    final changes doneee

--- src/Components/MainSlider.jsx
+++ src/Components/MainSlider.jsx
@@ -6,7 +6,8 @@ import getSliders from "../api/getSlider";
 import { url } from "../helper/url";
 import { useNavigate } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
-
+import Carousel from "react-bootstrap/Carousel";
+import "bootstrap/dist/css/bootstrap.min.css";
 function MainSlider() {
   const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1000px)" });
   const isDekstop = useMediaQuery({ query: "(min-width: 1001px)" });
@@ -82,95 +83,35 @@ function MainSlider() {
       )}
 
       {isTabletOrMobile && (
-        <div>
-          <div
-            id="animation-carousel"
-            class="relative w-full"
-            data-carousel="static"
-          >
-            <div class="relative h-56 overflow-hidden rounded-lg md:h-96 ">
-              {data
-                ?.filter((card) => card?.status)
-                .map((card, key) => (
+        <>
+          <Carousel data-bs-theme="dark">
+            {data
+              ?.filter((card) => card?.status)
+              .map((card, key) => (
+                <Carousel.Item key={key}>
+                  <img
+                    className="d-block w-100 h-[30vh]"
+                    src={url + "\\" + card.image.replace("uploads\\", "")}
+                    alt={`Image ${key}`}
+                  />
                   <div
-                    class="hidden duration-200 ease-linear"
-                    data-carousel-item
+                    className="flex flex-col w-[100vw] absolute"
+                    style={{
+                      left: "2%",
+                      top: "50%",
+                    }}
                   >
-                    <img
-                      src={url + "\\" + card.image.replace("uploads\\", "")}
-                      alt={`Image ${key}`}
-                      class="absolute bg-img block w-full  -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                    />
-                    {console.log(
-                      url + "\\" + card.image.replace("uploads\\", ""),
-                      "checkimage"
-                    )}
-                    <h1
-                      className="text-white  text-xl font-bold absolute"
-                      style={{
-                        left: "2%",
-                        top: "67%",
-                      }}
-                    >
+                    <h1 className="text-white banner-textt text-4xl font-bold absolute">
                       {card?.title}
                     </h1>
                     <BannerButtons
                       onWatch={() => navigate(`/live/${card._id}`)}
                     />
                   </div>
-                ))}
-            </div>
-
-            <button
-              type="button"
-              class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-              data-carousel-prev
-            >
-              <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg
-                  class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 6 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M5 1 1 5l4 4"
-                  />
-                </svg>
-                <span class="sr-only">Previous</span>
-              </span>
-            </button>
-            <button
-              type="button"
-              class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-              data-carousel-next
-            >
-              <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg
-                  class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 6 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="m1 9 4-4-4-4"
-                  />
-                </svg>
-                <span class="sr-only">Next</span>
-              </span>
-            </button>
-          </div>
-        </div>
+                </Carousel.Item>
+              ))}
+          </Carousel>
+        </>
       )}
     </>
   );
