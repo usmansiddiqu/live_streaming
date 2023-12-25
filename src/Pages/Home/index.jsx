720669f217c836142ac04695976a80d25dcbc3d2
Muhammad-Zaid-Yousaf <zaid.yousaf@venturenox.com>
2023-12-26 03:32:57 +0500

    fixes

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -19,33 +19,37 @@ function Home() {
   return (
     <>
       <div className="bg-[#130A2D] h-[90rem] main-home-page">
-        <MainSlider />
-        <div className="nfl-view">
-          <SliderHeader title="NFL Live" link="nfl" />
+        <div>
+          <MainSlider />
+          <div className="nfl-view">
+            <SliderHeader title="NFL Live" link="nfl" />
+          </div>
+          <CardSlider
+            data={data.filter((card) => {
+              return card.channel.TVCategory.name == "NFL";
+            })}
+            type="NFL"
+          />
+          <SliderHeader title="NHL Live" link="nhl" />
+          <CardSlider
+            data={data.filter((card) => card.channel.TVCategory.name == "NHL")}
+            type="NHL"
+          />
+          <SliderHeader title="NBA Live" link="nba" />
+          <CardSlider
+            data={data.filter((card) => card.channel.TVCategory.name == "NBA")}
+            type="NBL"
+          />
+          <SliderHeader title="MLB Live" link="mlb" />
+          <CardSlider
+            data={data.filter((card) => card.channel.TVCategory.name == "MLB")}
+            type="MLB"
+          />
         </div>
-        <CardSlider
-          data={data.filter((card) => {
-            return card.channel.TVCategory.name == "NFL";
-          })}
-          type="NFL"
-        />
-        <SliderHeader title="NHL Live" link="nhl" />
-        <CardSlider
-          data={data.filter((card) => card.channel.TVCategory.name == "NHL")}
-          type="NHL"
-        />
-        <SliderHeader title="NBA Live" link="nba" />
-        <CardSlider
-          data={data.filter((card) => card.channel.TVCategory.name == "NBA")}
-          type="NBL"
-        />
-        <SliderHeader title="MLB Live" link="mlb" />
-        <CardSlider
-          data={data.filter((card) => card.channel.TVCategory.name == "MLB")}
-          type="MLB"
-        />
+        <div className="mt-3">
+          <Footer />
+        </div>
       </div>
-      <Footer />
     </>
   );
 }
