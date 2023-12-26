41a919b21e168dff3f1703b831b67fe2f9992ce9
Muhammad-Zaid-Yousaf <zaid.yousaf@venturenox.com>
2023-12-26 16:40:22 +0500

    fixes

--- src/Components/Common/ErrorComponent.jsx
+++ src/Components/Common/ErrorComponent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function ErrorComponent({ message }) {
   return (
-    <div className="bg-[#F8D7DA] h-[auto] w-[auto] p-5 rounded mb-5">
+    <div className="bg-[#F8D7DA] h-[auto] w-[auto] p-2 rounded mb-5 error-box">
       <span className="text-[#D10B29]">{message}</span>
     </div>
   );
