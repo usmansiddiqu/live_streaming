ac8feb2d4ec89dd530c0018de3e07e0646f5a6f9
DawoodAli23 <tatwatatwa7@gmail.com>
2024-01-20 00:31:56 +0500

    another fix

--- src/Components/MainSlider.jsx
+++ src/Components/MainSlider.jsx
@@ -42,7 +42,7 @@ function MainSlider() {
         >
           <Splide
             options={{
-              start: 1,
+              start: 2,
               perPage: 1,
               perMove: 1,
               pagination: false,
@@ -54,9 +54,8 @@ function MainSlider() {
               width: 1400,
               height: 450,
               arrows: false,
-              preloadPages: 3,
+              preloadPages: 2,
               lazyLoad: "nearby",
-              speed: "150",
             }}
             style={{
               borderRadius: "20px",
