357459a640106f639e2c0ea8a6b856fade5ad1d2
Muhammad-Zaid-Yousaf <zaid.yousaf@venturenox.com>
2024-02-03 01:10:07 +0500

    fixes

--- src/api/canView.js
+++ src/api/canView.js
@@ -1,21 +1,6 @@
 const { default: axiosInstance } = require(".");
 
-<<<<<<< Updated upstream
 const canView = async () => {
   return await axiosInstance.get("/payment/canView");
-=======
-<<<<<<< Updated upstream
-const canView = async (id) => {
-  return await axiosInstance.get(`/payment/canView/${id}`);
-=======
-<<<<<<< Updated upstream
-const canView = async () => {
-  return await axiosInstance.get("/payment/canView");
-=======
-const canView = async (id) => {
-  return await axiosInstance.get(`/payment/canView`);
->>>>>>> Stashed changes
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 };
 export default canView;
