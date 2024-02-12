5782c7477618557f5adab7d6a0c8b3536a8d257e
DawoodAli23 <tatwatatwa7@gmail.com>
2024-02-12 23:27:46 +0500

    moved chat position

--- src/Components/crisp.jsx
+++ src/Components/crisp.jsx
@@ -5,6 +5,7 @@ import { Crisp } from "crisp-sdk-web";
 class CrispChat extends Component {
   componentDidMount() {
     Crisp.configure("5e1804e1-d295-42ef-abb5-ec3a7f4e98eb");
+    Crisp.setPosition("left");
   }
 
   render() {
