0d0cc7728a15acecc8f8b34afbfd17a38ad84fb9
dawoodali23 <tatwatatwa007@gmail.com>
2024-09-29 02:28:51 +0500

    fix

--- src/api/addPayment.js
+++ src/api/addPayment.js
@@ -1,7 +1,7 @@
 const { default: axiosInstance } = require(".");
 
 const createPayment = async (data) => {
-  return await axiosInstance.post(`/payment/payou/${data.package_id}`);
+  return await axiosInstance.get(`/payment/payou/${data.package_id}`);
   // return await axiosInstance.post("/payment", data);
 };
 export default createPayment;
